e1f64aa96a1df420a60a5c540b92ad725667837a
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2022-02-09 12:49:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double S = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите n ");
+        int n = in.nextInt();
+        // проверка отрицательности n
+        if (n > 0){
+            System.out.println("n должен быть положительным");
+        }
+        System.out.println("Введите x");
+        int x = in.nextInt();
+        for (var j = 0; j <= n; j++) {
+            double e = ((-1) ^ n) * ((x) ^ 2 * n) / (getFactorial(n));
+            S = S + e;
+        }
+        System.out.println(S);
+    }
+    //Считает факториал
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
